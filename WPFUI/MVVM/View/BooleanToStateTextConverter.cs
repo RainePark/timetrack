a26480f18a2b4632689173403f5522227dedf08c
Raine <rainep14.rp@gmail.com>
2023-04-26 00:17:46 +1000

    Automatically block status text based on ToggleButton

---
+++ WPFUI/MVVM/View/BooleanToStateTextConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace WPFUI.MVVM.View;
+using System;
+using System.Windows.Data;
+
+public class BooleanToStateTextConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is Boolean)
+        {
+            return (bool)value ? "Enabled" : "Disabled";
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is string)
+        {
+            return ((string)value) == "Enabled";
+        }
+
+        return value;
+    }
+}
