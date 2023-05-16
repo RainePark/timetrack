d3fb5388d34cf863e63809bd641c1e50e4e3f7fd
Raine <rainepark1@gmail.com>
2023-05-16 18:43:01 +1000

    Auto update selected menu

--- WPFUI/MVVM/ViewModel/MainViewModel.cs
+++ WPFUI/MVVM/ViewModel/MainViewModel.cs
@@ -3,6 +3,8 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using WPFUI.Core;
 
@@ -10,7 +12,7 @@ namespace WPFUI.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
-        public ICommand SelectPageCommand => new RelayCommand(SelectPage);
+        public ICommand SelectPageCommand { get; set; }
 
         private Dictionary<PageName, IPage> Pages { get; }
 
@@ -20,12 +22,16 @@ namespace WPFUI.MVVM.ViewModel
             get => this._selectedPage;
             set 
             { 
-                this._selectedPage = value; 
-                OnPropertyChanged();
+                if (_selectedPage != value)
+                {
+                    this._selectedPage = value; 
+                    OnPropertyChanged();
+                }
             }
         }
         public MainViewModel()
         {
+            SelectPageCommand = new RelayCommand(SelectPage);
             this.Pages = new Dictionary<PageName, IPage>
             {
                 { PageName.Dashboard, new DashboardViewModel() },
@@ -39,16 +45,47 @@ namespace WPFUI.MVVM.ViewModel
             _programUsageModel = new ProgramUsageModel();
             _programUsageModel.PropertyChanged += ProgramUsageModel_PropertyChanged;
         }
-        
+
         public void SelectPage(object param)
         {
-            if (param is PageName pageName 
+            if (param is PageName pageName
                 && this.Pages.TryGetValue(pageName, out IPage _selectedPage))
             {
                 this.SelectedPage = _selectedPage;
+                UpdateRadioButtonIsChecked();
             }
         }
-        
+
+        public void UpdateRadioButtonIsChecked()
+        {
+            var mainWindow = Application.Current.MainWindow;
+            if (this.SelectedPage.PageTitle == "Dashboard")
+            {
+                var dashboardSidebarRadioButton = (RadioButton)mainWindow.FindName("DashboardSidebarRadioButton");
+                dashboardSidebarRadioButton.IsChecked = true;
+            }
+            else if (this.SelectedPage.PageTitle == "Usage")
+            {
+                var usageSidebarRadioButton = (RadioButton)mainWindow.FindName("UsageSidebarRadioButton");
+                usageSidebarRadioButton.IsChecked = true;
+            }
+            else if (this.SelectedPage.PageTitle == "Blocks")
+            {
+                var blocksSidebarRadioButton = (RadioButton)mainWindow.FindName("BlocksSidebarRadioButton");
+                blocksSidebarRadioButton.IsChecked = true;
+            }
+            else if (this.SelectedPage.PageTitle == "Settings")
+            {
+                var settingsSidebarRadioButton = (RadioButton)mainWindow.FindName("SettingsSidebarRadioButton");
+                settingsSidebarRadioButton.IsChecked = true;
+            }
+            else
+            {
+                var dashboardSidebarRadioButton = (RadioButton)mainWindow.FindName("DashboardSidebarRadioButton");
+                dashboardSidebarRadioButton.IsChecked = true;
+            }
+        }
+
         /*TESTING SECTION*/
         private ProgramUsageModel _programUsageModel;
         
