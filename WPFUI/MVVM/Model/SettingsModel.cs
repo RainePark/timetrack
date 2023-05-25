0e490cdfef8eb3ea75d446e3b4688b734564c522
Raine <rainepark1@gmail.com>
2023-05-25 18:40:20 +1000

    Create settings model

--- WPFUI/MVVM/Model/SettingsModel.cs
+++ WPFUI/MVVM/Model/SettingsModel.cs
@@ -1,6 +1,34 @@
-﻿namespace WPFUI.MVVM.Model;
+﻿using System.ComponentModel;
+using System.IO;
+using Newtonsoft.Json;
+using WPFUI.Core;
 
-public class SettingsModel
+namespace WPFUI.MVVM.Model;
+
+public class SettingsModel : ObservableObject
 {
-    
+    public SettingsModel()
+    {
+
+    }
+
+    public static Settings GetUserSettings()
+    {
+        return (JsonConvert.DeserializeObject<Settings>(File.ReadAllText("user\\settings.json")));
+    }
+
+    public static void WriteSettings(Settings settings)
+    {
+        using (StreamWriter writer = new StreamWriter("user\\settings.json"))
+        {
+            writer.WriteLine(JsonConvert.SerializeObject(settings));
+        }
+    }
+}
+
+public class Settings
+{
+    public string UserName;
+    public bool SystemApps;
+    public string BlockType;
 }
